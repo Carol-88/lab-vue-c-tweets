6cb2cd8e05efd64400827191fc9ee5caaf6828a9
Carol-88 <gralleta@gmail.com>
2024-05-01 21:56:26 +0200

    iteration2

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tweet">
     <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
+      :src="tweet.user.image"
       class="profile"
       alt="profile"
     />
@@ -9,16 +9,15 @@
     <div class="body">
       <div class="top">
         <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
+          <span class="name">{{tweet.user.name}}</span>
+          <span class="handle">{{tweet.user.handle}}</span>
         </span>
 
-        <span class="timestamp">Nov 30, 2020</span>
+        <span class="timestamp">{{tweet.timestamp}}</span>
       </div>
 
       <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
+        {{tweet.message}}
       </p>
 
       <div class="actions">
@@ -33,6 +32,20 @@
     <i class="fas fa-ellipsis-h"></i>
   </div>
 </template>
+
+
+<script setup>
+
+import { defineProps } from 'vue';
+
+const props = defineProps({
+ tweet: {
+    type: Object,
+    required: true
+ }
+});
+
+</script>
 
 <style scoped>
 a {
